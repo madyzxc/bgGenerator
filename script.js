c05a2abffe8fa3a69ed27f71ee28ef01cafba01e
Mohamed Ibrahim <cs.mohamed.elassal@gmail.com>
2020-03-10 18:16:03 +0400

    remove bundles

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
-var randomize = document.getElementById("randomize");
+var css = document.querySelector('h3');
+var color1 = document.querySelector('.color1');
+var color2 = document.querySelector('.color2');
+var body = document.getElementById('gradient');
+var randomize = document.getElementById('randomize');
 
 function setGradient() {
   body.style.background =
-    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+    'linear-gradient(to right, ' + color1.value + ', ' + color2.value + ')';
 
-  css.textContent = body.style.background + ";";
+  css.textContent = body.style.background + ';';
 }
 
 function onLoadBody() {
   setGradient();
-  css.textContent = body.style.background + ";";
+  css.textContent = body.style.background + ';';
 }
 
 function getRandomInt(max) {
@@ -22,11 +22,11 @@ function getRandomInt(max) {
 
 function componentToHex(c) {
   var hex = c.toString(16);
-  return hex.length == 1 ? "0" + hex : hex;
+  return hex.length == 1 ? '0' + hex : hex;
 }
 
 function rgbToHex(r, g, b) {
-  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+  return '#' + componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
 
 function randomizeValues() {
@@ -45,10 +45,10 @@ function randomizeValues() {
   setGradient();
 }
 
-window.addEventListener("load", onLoadBody);
+window.addEventListener('load', onLoadBody);
 
-color1.addEventListener("input", setGradient);
+color1.addEventListener('input', setGradient);
 
-color2.addEventListener("input", setGradient);
+color2.addEventListener('input', setGradient);
 
-randomize.addEventListener("click", randomizeValues);
+randomize.addEventListener('click', randomizeValues);
